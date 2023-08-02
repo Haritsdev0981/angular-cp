7868164057c9551baeb4c0ec2a70c8c71e9c38b6
Harits <haritsdeveloper@gmail.com>
2023-08-03 02:31:54 +0700

    add-section

--- src/app/presentation/presentation.module.ts
+++ src/app/presentation/presentation.module.ts
@@ -6,15 +6,19 @@ import { CommonModule } from "@angular/common";
 import { PresentationComponent } from "./presentation.component";
 import { CarouselModule } from "ngx-bootstrap/carousel";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TabsModule } from "ngx-bootstrap/tabs";
 import { CollapseModule } from "ngx-bootstrap/collapse";
 import { LayananComponent } from "../components/layanan/layanan.component";
+import { PricingComponent } from "../components/pricing/pricing.component";
+import { TestimonialComponent } from "../components/testimonial/testimonial.component";
 @NgModule({
-  declarations: [PresentationComponent, LayananComponent],
+  declarations: [PresentationComponent, LayananComponent, PricingComponent, TestimonialComponent],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule,
     BrowserAnimationsModule,
+    TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     CarouselModule.forRoot()
